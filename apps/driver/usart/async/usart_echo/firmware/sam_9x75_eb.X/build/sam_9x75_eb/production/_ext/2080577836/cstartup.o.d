0779760dc6ca573330561a2ce0b7efd6458658a2
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2021-12-22 00:43:45 +0530

    [APPS] Added driver_usart_async_usart_echo_sam_9x75_eb demo
    MH3-61380

---
+++ apps/driver/usart/async/usart_echo/firmware/sam_9x75_eb.X/build/sam_9x75_eb/production/_ext/2080577836/cstartup.o.d
@@ -0,0 +1,5 @@
+build/sam_9x75_eb/production/_ext/2080577836/cstartup.o:  \
+ ../src/config/sam_9x75_eb/cstartup.S 
+build/sam_9x75_eb/production/_ext/2080577836/cstartup.o:  \
+  \
+../src/config/sam_9x75_eb/cstartup.S  \
