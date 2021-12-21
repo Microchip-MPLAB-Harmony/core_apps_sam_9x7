31c64dd57959e585f5d6809f1fab3881108ca5b2
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2021-12-21 23:32:58 +0530

    [APPS] Added rtos_freertos_basic_freertos_sam_9x75_eb demo
    MH3-61374

---
+++ apps/rtos/freertos/basic_freertos/firmware/sam_9x75_eb.X/build/sam_9x75_eb/production/_ext/404212886/FreeRTOS_tasks.o.d
@@ -0,0 +1,41 @@
+build/sam_9x75_eb/production/_ext/404212886/FreeRTOS_tasks.o: \
+ ../src/third_party/rtos/FreeRTOS/Source/FreeRTOS_tasks.c \
+ ../src/third_party/rtos/FreeRTOS/Source/include/FreeRTOS.h \
+ ../src/config/sam_9x75_eb/FreeRTOSConfig.h \
+ ../src/third_party/rtos/FreeRTOS/Source/include/projdefs.h \
+ ../src/third_party/rtos/FreeRTOS/Source/include/portable.h \
+ ../src/third_party/rtos/FreeRTOS/Source/include/deprecated_definitions.h \
+ ../src/third_party/rtos/FreeRTOS/Source/portable/XC32/SAM/ARM926/portmacro.h \
+ ../src/config/sam_9x75_eb/toolchain_specifics.h \
+ ../src/third_party/rtos/FreeRTOS/Source/include/mpu_wrappers.h \
+ ../src/third_party/rtos/FreeRTOS/Source/include/task.h \
+ ../src/third_party/rtos/FreeRTOS/Source/include/list.h \
+ ../src/third_party/rtos/FreeRTOS/Source/include/timers.h \
+ ../src/third_party/rtos/FreeRTOS/Source/include/task.h \
+ ../src/third_party/rtos/FreeRTOS/Source/include/stack_macros.h
+
+../src/third_party/rtos/FreeRTOS/Source/include/FreeRTOS.h:
+
+../src/config/sam_9x75_eb/FreeRTOSConfig.h:
+
+../src/third_party/rtos/FreeRTOS/Source/include/projdefs.h:
+
+../src/third_party/rtos/FreeRTOS/Source/include/portable.h:
+
+../src/third_party/rtos/FreeRTOS/Source/include/deprecated_definitions.h:
+
+../src/third_party/rtos/FreeRTOS/Source/portable/XC32/SAM/ARM926/portmacro.h:
+
+../src/config/sam_9x75_eb/toolchain_specifics.h:
+
+../src/third_party/rtos/FreeRTOS/Source/include/mpu_wrappers.h:
+
+../src/third_party/rtos/FreeRTOS/Source/include/task.h:
+
+../src/third_party/rtos/FreeRTOS/Source/include/list.h:
+
+../src/third_party/rtos/FreeRTOS/Source/include/timers.h:
+
+../src/third_party/rtos/FreeRTOS/Source/include/task.h:
+
+../src/third_party/rtos/FreeRTOS/Source/include/stack_macros.h:
---
+++ apps/rtos/freertos/basic_freertos/firmware/sam_9x75_eb.X/build/sam_9x75_eb/production/_ext/601890357/plib_pit.o.d
@@ -0,0 +1,367 @@
+build/sam_9x75_eb/production/_ext/601890357/plib_pit.o: \
+ ../src/config/sam_9x75_eb/peripheral/pit/plib_pit.c \
+ ../src/config/sam_9x75_eb/peripheral/pit/plib_pit.h \
+ ../src/config/sam_9x75_eb/device.h ../src/packs/SAM9X75_DFP/sam9x75.h \
+ ../src/packs/SAM9X75_DFP/component/adc.h \
+ ../src/packs/SAM9X75_DFP/component/aes.h \
+ ../src/packs/SAM9X75_DFP/component/aic.h \
+ ../src/packs/SAM9X75_DFP/component/bsc.h \
+ ../src/packs/SAM9X75_DFP/component/classd.h \
+ ../src/packs/SAM9X75_DFP/component/csi.h \
+ ../src/packs/SAM9X75_DFP/component/csi2dc.h \
+ ../src/packs/SAM9X75_DFP/component/dbgu.h \
+ ../src/packs/SAM9X75_DFP/component/dsi.h \
+ ../src/packs/SAM9X75_DFP/component/flexcom.h \
+ ../src/packs/SAM9X75_DFP/component/gfx2d.h \
+ ../src/packs/SAM9X75_DFP/component/gmac.h \
+ ../src/packs/SAM9X75_DFP/component/gpbr.h \
+ ../src/packs/SAM9X75_DFP/component/i2smcc.h \
+ ../src/packs/SAM9X75_DFP/component/isc.h \
+ ../src/packs/SAM9X75_DFP/component/lcdc.h \
+ ../src/packs/SAM9X75_DFP/component/lvdsc.h \
+ ../src/packs/SAM9X75_DFP/component/matrix.h \
+ ../src/packs/SAM9X75_DFP/component/mcan.h \
+ ../src/packs/SAM9X75_DFP/component/mpddrc.h \
+ ../src/packs/SAM9X75_DFP/component/otpc.h \
+ ../src/packs/SAM9X75_DFP/component/pio.h \
+ ../src/packs/SAM9X75_DFP/component/pit.h \
+ ../src/packs/SAM9X75_DFP/component/pit64b.h \
+ ../src/packs/SAM9X75_DFP/component/pmc.h \
+ ../src/packs/SAM9X75_DFP/component/pmecc.h \
+ ../src/packs/SAM9X75_DFP/component/pmerrloc.h \
+ ../src/packs/SAM9X75_DFP/component/puf.h \
+ ../src/packs/SAM9X75_DFP/component/pwm.h \
+ ../src/packs/SAM9X75_DFP/component/qspi.h \
+ ../src/packs/SAM9X75_DFP/component/rstc.h \
+ ../src/packs/SAM9X75_DFP/component/rtc.h \
+ ../src/packs/SAM9X75_DFP/component/rtt.h \
+ ../src/packs/SAM9X75_DFP/component/sckc.h \
+ ../src/packs/SAM9X75_DFP/component/sdmmc.h \
+ ../src/packs/SAM9X75_DFP/component/sfr.h \
+ ../src/packs/SAM9X75_DFP/component/sha.h \
+ ../src/packs/SAM9X75_DFP/component/shdwc.h \
+ ../src/packs/SAM9X75_DFP/component/smc.h \
+ ../src/packs/SAM9X75_DFP/component/ssc.h \
+ ../src/packs/SAM9X75_DFP/component/syscwp.h \
+ ../src/packs/SAM9X75_DFP/component/tc.h \
+ ../src/packs/SAM9X75_DFP/component/tdes.h \
+ ../src/packs/SAM9X75_DFP/component/trng.h \
+ ../src/packs/SAM9X75_DFP/component/udphs.h \
+ ../src/packs/SAM9X75_DFP/component/uhpfs.h \
+ ../src/packs/SAM9X75_DFP/component/uhphs.h \
+ ../src/packs/SAM9X75_DFP/component/wdt.h \
+ ../src/packs/SAM9X75_DFP/component/xdmac.h \
+ ../src/packs/SAM9X75_DFP/instance/adc.h \
+ ../src/packs/SAM9X75_DFP/instance/aes.h \
+ ../src/packs/SAM9X75_DFP/instance/aic.h \
+ ../src/packs/SAM9X75_DFP/instance/bsc.h \
+ ../src/packs/SAM9X75_DFP/instance/classd.h \
+ ../src/packs/SAM9X75_DFP/instance/csi2dc.h \
+ ../src/packs/SAM9X75_DFP/instance/dbgu.h \
+ ../src/packs/SAM9X75_DFP/instance/flexcom0.h \
+ ../src/packs/SAM9X75_DFP/instance/flexcom1.h \
+ ../src/packs/SAM9X75_DFP/instance/flexcom10.h \
+ ../src/packs/SAM9X75_DFP/instance/flexcom11.h \
+ ../src/packs/SAM9X75_DFP/instance/flexcom12.h \
+ ../src/packs/SAM9X75_DFP/instance/flexcom2.h \
+ ../src/packs/SAM9X75_DFP/instance/flexcom3.h \
+ ../src/packs/SAM9X75_DFP/instance/flexcom4.h \
+ ../src/packs/SAM9X75_DFP/instance/flexcom5.h \
+ ../src/packs/SAM9X75_DFP/instance/flexcom6.h \
+ ../src/packs/SAM9X75_DFP/instance/flexcom7.h \
+ ../src/packs/SAM9X75_DFP/instance/flexcom8.h \
+ ../src/packs/SAM9X75_DFP/instance/flexcom9.h \
+ ../src/packs/SAM9X75_DFP/instance/gfx2d.h \
+ ../src/packs/SAM9X75_DFP/instance/gmac.h \
+ ../src/packs/SAM9X75_DFP/instance/gpbr.h \
+ ../src/packs/SAM9X75_DFP/instance/i2smcc.h \
+ ../src/packs/SAM9X75_DFP/instance/isc.h \
+ ../src/packs/SAM9X75_DFP/instance/lvdsc.h \
+ ../src/packs/SAM9X75_DFP/instance/matrix.h \
+ ../src/packs/SAM9X75_DFP/instance/mcan0.h \
+ ../src/packs/SAM9X75_DFP/instance/mcan1.h \
+ ../src/packs/SAM9X75_DFP/instance/mpddrc.h \
+ ../src/packs/SAM9X75_DFP/instance/otpc.h \
+ ../src/packs/SAM9X75_DFP/instance/pioa.h \
+ ../src/packs/SAM9X75_DFP/instance/piob.h \
+ ../src/packs/SAM9X75_DFP/instance/pioc.h \
+ ../src/packs/SAM9X75_DFP/instance/piod.h \
+ ../src/packs/SAM9X75_DFP/instance/pit.h \
+ ../src/packs/SAM9X75_DFP/instance/pit64b0.h \
+ ../src/packs/SAM9X75_DFP/instance/pit64b1.h \
+ ../src/packs/SAM9X75_DFP/instance/pmc.h \
+ ../src/packs/SAM9X75_DFP/instance/pmecc.h \
+ ../src/packs/SAM9X75_DFP/instance/pmerrloc.h \
+ ../src/packs/SAM9X75_DFP/instance/puf.h \
+ ../src/packs/SAM9X75_DFP/instance/pwm.h \
+ ../src/packs/SAM9X75_DFP/instance/qspi.h \
+ ../src/packs/SAM9X75_DFP/instance/rstc.h \
+ ../src/packs/SAM9X75_DFP/instance/rtc.h \
+ ../src/packs/SAM9X75_DFP/instance/rtt.h \
+ ../src/packs/SAM9X75_DFP/instance/sckc.h \
+ ../src/packs/SAM9X75_DFP/instance/sdmmc0.h \
+ ../src/packs/SAM9X75_DFP/instance/sdmmc1.h \
+ ../src/packs/SAM9X75_DFP/instance/sfr.h \
+ ../src/packs/SAM9X75_DFP/instance/sha.h \
+ ../src/packs/SAM9X75_DFP/instance/shdwc.h \
+ ../src/packs/SAM9X75_DFP/instance/smc.h \
+ ../src/packs/SAM9X75_DFP/instance/ssc.h \
+ ../src/packs/SAM9X75_DFP/instance/syscwp.h \
+ ../src/packs/SAM9X75_DFP/instance/tc0.h \
+ ../src/packs/SAM9X75_DFP/instance/tc1.h \
+ ../src/packs/SAM9X75_DFP/instance/tdes.h \
+ ../src/packs/SAM9X75_DFP/instance/trng.h \
+ ../src/packs/SAM9X75_DFP/instance/udphs.h \
+ ../src/packs/SAM9X75_DFP/instance/uhphs_ehci.h \
+ ../src/packs/SAM9X75_DFP/instance/uhphs_ohci.h \
+ ../src/packs/SAM9X75_DFP/instance/wdt.h \
+ ../src/packs/SAM9X75_DFP/instance/xdmac.h \
+ ../src/packs/SAM9X75_DFP/instance/xlcdc.h \
+ ../src/packs/SAM9X75_DFP/pio/sam9x75.h \
+ ../src/config/sam_9x75_eb/device_cache.h \
+ ../src/config/sam_9x75_eb/peripheral/mmu/plib_mmu.h \
+ ../src/config/sam_9x75_eb/toolchain_specifics.h
+
+../src/config/sam_9x75_eb/peripheral/pit/plib_pit.h:
+
+../src/config/sam_9x75_eb/device.h:
+
+../src/packs/SAM9X75_DFP/sam9x75.h:
+
+../src/packs/SAM9X75_DFP/component/adc.h:
+
+../src/packs/SAM9X75_DFP/component/aes.h:
+
+../src/packs/SAM9X75_DFP/component/aic.h:
+
+../src/packs/SAM9X75_DFP/component/bsc.h:
+
+../src/packs/SAM9X75_DFP/component/classd.h:
+
+../src/packs/SAM9X75_DFP/component/csi.h:
+
+../src/packs/SAM9X75_DFP/component/csi2dc.h:
+
+../src/packs/SAM9X75_DFP/component/dbgu.h:
+
+../src/packs/SAM9X75_DFP/component/dsi.h:
+
+../src/packs/SAM9X75_DFP/component/flexcom.h:
+
+../src/packs/SAM9X75_DFP/component/gfx2d.h:
+
+../src/packs/SAM9X75_DFP/component/gmac.h:
+
+../src/packs/SAM9X75_DFP/component/gpbr.h:
+
+../src/packs/SAM9X75_DFP/component/i2smcc.h:
+
+../src/packs/SAM9X75_DFP/component/isc.h:
+
+../src/packs/SAM9X75_DFP/component/lcdc.h:
+
+../src/packs/SAM9X75_DFP/component/lvdsc.h:
+
+../src/packs/SAM9X75_DFP/component/matrix.h:
+
+../src/packs/SAM9X75_DFP/component/mcan.h:
+
+../src/packs/SAM9X75_DFP/component/mpddrc.h:
+
+../src/packs/SAM9X75_DFP/component/otpc.h:
+
+../src/packs/SAM9X75_DFP/component/pio.h:
+
+../src/packs/SAM9X75_DFP/component/pit.h:
+
+../src/packs/SAM9X75_DFP/component/pit64b.h:
+
+../src/packs/SAM9X75_DFP/component/pmc.h:
+
+../src/packs/SAM9X75_DFP/component/pmecc.h:
+
+../src/packs/SAM9X75_DFP/component/pmerrloc.h:
+
+../src/packs/SAM9X75_DFP/component/puf.h:
+
+../src/packs/SAM9X75_DFP/component/pwm.h:
+
+../src/packs/SAM9X75_DFP/component/qspi.h:
+
+../src/packs/SAM9X75_DFP/component/rstc.h:
+
+../src/packs/SAM9X75_DFP/component/rtc.h:
+
+../src/packs/SAM9X75_DFP/component/rtt.h:
+
+../src/packs/SAM9X75_DFP/component/sckc.h:
+
+../src/packs/SAM9X75_DFP/component/sdmmc.h:
+
+../src/packs/SAM9X75_DFP/component/sfr.h:
+
+../src/packs/SAM9X75_DFP/component/sha.h:
+
+../src/packs/SAM9X75_DFP/component/shdwc.h:
+
+../src/packs/SAM9X75_DFP/component/smc.h:
+
+../src/packs/SAM9X75_DFP/component/ssc.h:
+
+../src/packs/SAM9X75_DFP/component/syscwp.h:
+
+../src/packs/SAM9X75_DFP/component/tc.h:
+
+../src/packs/SAM9X75_DFP/component/tdes.h:
+
+../src/packs/SAM9X75_DFP/component/trng.h:
+
+../src/packs/SAM9X75_DFP/component/udphs.h:
+
+../src/packs/SAM9X75_DFP/component/uhpfs.h:
+
+../src/packs/SAM9X75_DFP/component/uhphs.h:
+
+../src/packs/SAM9X75_DFP/component/wdt.h:
+
+../src/packs/SAM9X75_DFP/component/xdmac.h:
+
+../src/packs/SAM9X75_DFP/instance/adc.h:
+
+../src/packs/SAM9X75_DFP/instance/aes.h:
+
+../src/packs/SAM9X75_DFP/instance/aic.h:
+
+../src/packs/SAM9X75_DFP/instance/bsc.h:
+
+../src/packs/SAM9X75_DFP/instance/classd.h:
+
+../src/packs/SAM9X75_DFP/instance/csi2dc.h:
+
+../src/packs/SAM9X75_DFP/instance/dbgu.h:
+
+../src/packs/SAM9X75_DFP/instance/flexcom0.h:
+
+../src/packs/SAM9X75_DFP/instance/flexcom1.h:
+
+../src/packs/SAM9X75_DFP/instance/flexcom10.h:
+
+../src/packs/SAM9X75_DFP/instance/flexcom11.h:
+
+../src/packs/SAM9X75_DFP/instance/flexcom12.h:
+
+../src/packs/SAM9X75_DFP/instance/flexcom2.h:
+
+../src/packs/SAM9X75_DFP/instance/flexcom3.h:
+
+../src/packs/SAM9X75_DFP/instance/flexcom4.h:
+
+../src/packs/SAM9X75_DFP/instance/flexcom5.h:
+
+../src/packs/SAM9X75_DFP/instance/flexcom6.h:
+
+../src/packs/SAM9X75_DFP/instance/flexcom7.h:
+
+../src/packs/SAM9X75_DFP/instance/flexcom8.h:
+
+../src/packs/SAM9X75_DFP/instance/flexcom9.h:
+
+../src/packs/SAM9X75_DFP/instance/gfx2d.h:
+
+../src/packs/SAM9X75_DFP/instance/gmac.h:
+
+../src/packs/SAM9X75_DFP/instance/gpbr.h:
+
+../src/packs/SAM9X75_DFP/instance/i2smcc.h:
+
+../src/packs/SAM9X75_DFP/instance/isc.h:
+
+../src/packs/SAM9X75_DFP/instance/lvdsc.h:
+
+../src/packs/SAM9X75_DFP/instance/matrix.h:
+
+../src/packs/SAM9X75_DFP/instance/mcan0.h:
+
+../src/packs/SAM9X75_DFP/instance/mcan1.h:
+
+../src/packs/SAM9X75_DFP/instance/mpddrc.h:
+
+../src/packs/SAM9X75_DFP/instance/otpc.h:
+
+../src/packs/SAM9X75_DFP/instance/pioa.h:
+
+../src/packs/SAM9X75_DFP/instance/piob.h:
+
+../src/packs/SAM9X75_DFP/instance/pioc.h:
+
+../src/packs/SAM9X75_DFP/instance/piod.h:
+
+../src/packs/SAM9X75_DFP/instance/pit.h:
+
+../src/packs/SAM9X75_DFP/instance/pit64b0.h:
+
+../src/packs/SAM9X75_DFP/instance/pit64b1.h:
+
+../src/packs/SAM9X75_DFP/instance/pmc.h:
+
+../src/packs/SAM9X75_DFP/instance/pmecc.h:
+
+../src/packs/SAM9X75_DFP/instance/pmerrloc.h:
+
+../src/packs/SAM9X75_DFP/instance/puf.h:
+
+../src/packs/SAM9X75_DFP/instance/pwm.h:
+
+../src/packs/SAM9X75_DFP/instance/qspi.h:
+
+../src/packs/SAM9X75_DFP/instance/rstc.h:
+
+../src/packs/SAM9X75_DFP/instance/rtc.h:
+
+../src/packs/SAM9X75_DFP/instance/rtt.h:
+
+../src/packs/SAM9X75_DFP/instance/sckc.h:
+
+../src/packs/SAM9X75_DFP/instance/sdmmc0.h:
+
+../src/packs/SAM9X75_DFP/instance/sdmmc1.h:
+
+../src/packs/SAM9X75_DFP/instance/sfr.h:
+
+../src/packs/SAM9X75_DFP/instance/sha.h:
+
+../src/packs/SAM9X75_DFP/instance/shdwc.h:
+
+../src/packs/SAM9X75_DFP/instance/smc.h:
+
+../src/packs/SAM9X75_DFP/instance/ssc.h:
+
+../src/packs/SAM9X75_DFP/instance/syscwp.h:
+
+../src/packs/SAM9X75_DFP/instance/tc0.h:
+
+../src/packs/SAM9X75_DFP/instance/tc1.h:
+
+../src/packs/SAM9X75_DFP/instance/tdes.h:
+
+../src/packs/SAM9X75_DFP/instance/trng.h:
+
+../src/packs/SAM9X75_DFP/instance/udphs.h:
+
+../src/packs/SAM9X75_DFP/instance/uhphs_ehci.h:
+
+../src/packs/SAM9X75_DFP/instance/uhphs_ohci.h:
+
+../src/packs/SAM9X75_DFP/instance/wdt.h:
+
+../src/packs/SAM9X75_DFP/instance/xdmac.h:
+
+../src/packs/SAM9X75_DFP/instance/xlcdc.h:
+
+../src/packs/SAM9X75_DFP/pio/sam9x75.h:
+
+../src/config/sam_9x75_eb/device_cache.h:
+
+../src/config/sam_9x75_eb/peripheral/mmu/plib_mmu.h:
+
+../src/config/sam_9x75_eb/toolchain_specifics.h:
