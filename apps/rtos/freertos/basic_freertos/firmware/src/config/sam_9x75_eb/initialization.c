cd1677e91d7ada9d2de421aef856b36a503ae5ba
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2023-05-12 15:16:49 +0530

    [RTOS] Regenerated demo for rtos_freertos_basic_freertos_sam_9x75_eb
    MH3-76423

--- apps/rtos/freertos/basic_freertos/firmware/src/config/sam_9x75_eb/initialization.c
+++ apps/rtos/freertos/basic_freertos/firmware/src/config/sam_9x75_eb/initialization.c
@@ -62,6 +62,11 @@
 // Section: Driver Initialization Data
 // *****************************************************************************
 // *****************************************************************************
+/* Following MISRA-C rules are deviated in the below code block */
+/* MISRA C-2012 Rule 11.1 */
+/* MISRA C-2012 Rule 11.3 */
+/* MISRA C-2012 Rule 11.8 */
+
 
 
 // *****************************************************************************
@@ -142,7 +147,7 @@ static void SYSC_Disable( void )
 }
 
 
-
+/* MISRAC 2012 deviation block end */
 
 /*******************************************************************************
   Function:
@@ -156,6 +161,8 @@ static void SYSC_Disable( void )
 
 void SYS_Initialize ( void* data )
 {
+    /* MISRAC 2012 deviation block start */
+    /* MISRA C-2012 Rule 2.2 deviated in this file.  Deviation record ID -  H3_MISRAC_2012_R_2_2_DR_1 */
 
 	SYSC_Disable( );
 
@@ -180,14 +187,24 @@ void SYS_Initialize ( void* data )
 	BSP_Initialize();
 
 
+    /* MISRAC 2012 deviation block start */
+    /* Following MISRA-C rules deviated in this block  */
+    /* MISRA C-2012 Rule 11.3 - Deviation record ID - H3_MISRAC_2012_R_11_3_DR_1 */
+    /* MISRA C-2012 Rule 11.8 - Deviation record ID - H3_MISRAC_2012_R_11_8_DR_1 */
 
 
+
+
+    /* MISRAC 2012 deviation block end */
     TASK1_Initialize();
     TASK2_Initialize();
     TASK3_Initialize();
     TASK4_Initialize();
 
 
+
+
+    /* MISRAC 2012 deviation block end */
 
 }
 
