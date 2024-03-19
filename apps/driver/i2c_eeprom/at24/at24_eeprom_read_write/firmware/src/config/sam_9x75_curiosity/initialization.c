16ba7d01bd94a18dc3a6cff9afb6413229e4dfaa
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2024-03-19 15:15:30 +0530

    [DRV_I2C] Add driver_i2c_eeprom_at24_eeprom_read_write demo application for sam_9x75 curiosity
    MH3-86793
    
    Signed-off-by:  Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>

---
+++ apps/driver/i2c_eeprom/at24/at24_eeprom_read_write/firmware/src/config/sam_9x75_curiosity/initialization.c
@@ -0,0 +1,262 @@
+/*******************************************************************************
+  System Initialization File
+
+  File Name:
+    initialization.c
+
+  Summary:
+    This file contains source code necessary to initialize the system.
+
+  Description:
+    This file contains source code necessary to initialize the system.  It
+    implements the "SYS_Initialize" function, defines the configuration bits,
+    and allocates any necessary global system resources,
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+#include "configuration.h"
+#include "definitions.h"
+#include "device.h"
+
+
+// ****************************************************************************
+// ****************************************************************************
+// Section: Configuration Bits
+// ****************************************************************************
+// ****************************************************************************
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Driver Initialization Data
+// *****************************************************************************
+// *****************************************************************************
+/* Following MISRA-C rules are deviated in the below code block */
+/* MISRA C-2012 Rule 11.1 */
+/* MISRA C-2012 Rule 11.3 */
+/* MISRA C-2012 Rule 11.8 */
+// <editor-fold defaultstate="collapsed" desc="DRV_AT24 Initialization Data">
+
+/* I2C PLIB Interface Initialization for AT24 Driver */
+static const DRV_AT24_PLIB_INTERFACE drvAT24PlibAPI = {
+
+    /* I2C PLIB WriteRead function */
+    .writeRead = (DRV_AT24_PLIB_WRITE_READ)FLEXCOM7_TWI_WriteRead,
+
+    /* I2C PLIB Write function */
+    .write_t = (DRV_AT24_PLIB_WRITE)FLEXCOM7_TWI_Write,
+
+    /* I2C PLIB Read function */
+    .read_t = (DRV_AT24_PLIB_READ)FLEXCOM7_TWI_Read,
+
+    /* I2C PLIB Transfer Status function */
+    .isBusy = (DRV_AT24_PLIB_IS_BUSY)FLEXCOM7_TWI_IsBusy,
+
+    /* I2C PLIB Error Status function */
+    .errorGet = (DRV_AT24_PLIB_ERROR_GET)FLEXCOM7_TWI_ErrorGet,
+
+    /* I2C PLIB Callback Register */
+    .callbackRegister = (DRV_AT24_PLIB_CALLBACK_REGISTER)FLEXCOM7_TWI_CallbackRegister,
+};
+
+/* AT24 Driver Initialization Data */
+static const DRV_AT24_INIT drvAT24InitData =
+{
+    /* I2C PLIB API  interface*/
+    .i2cPlib = &drvAT24PlibAPI,
+
+    /* 7-bit I2C Slave address */
+    .slaveAddress = 0x54,
+
+    /* EEPROM Page Size in bytes */
+    .pageSize = DRV_AT24_EEPROM_PAGE_SIZE,
+
+    /* Total size of the EEPROM in bytes */
+    .flashSize = DRV_AT24_EEPROM_FLASH_SIZE,
+
+    /* AT24 Number of clients */
+    .numClients = DRV_AT24_CLIENTS_NUMBER_IDX,
+
+    .blockStartAddress =    0x0,
+};
+// </editor-fold>
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Data
+// *****************************************************************************
+// *****************************************************************************
+/* Structure to hold the object handles for the modules in the system. */
+SYSTEM_OBJECTS sysObj;
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Library/Stack Initialization Data
+// *****************************************************************************
+// *****************************************************************************
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Initialization
+// *****************************************************************************
+// *****************************************************************************
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Local initialization functions
+// *****************************************************************************
+// *****************************************************************************
+/*******************************************************************************
+  Function:
+    void SYSC_Disable ( void )
+
+  Summary:
+    Disables ununsed SYSC peripherals
+
+  Remarks:
+ */
+static void SYSC_Disable( void )
+{
+    //save context and disable write protection
+    uint32_t sysc_wpmr = SYSCWP_REGS->SYSCWP_SYSC_WPMR &
+      (SYSCWP_SYSC_WPMR_WPEN_Msk | SYSCWP_SYSC_WPMR_WPITEN_Msk);
+    SYSCWP_REGS->SYSCWP_SYSC_WPMR = SYSCWP_SYSC_WPMR_WPKEY_PASSWD &
+                                    ~(SYSCWP_SYSC_WPMR_WPITEN_Msk |
+                                    SYSCWP_SYSC_WPMR_WPITEN_Msk);
+
+
+    /* ----------------------------   RTC  -------------------------------*/
+    //Disable interrupts
+    RTC_REGS->RTC_IDR = RTC_IDR_Msk;
+
+    //Clear interrupt status
+    RTC_REGS->RTC_SCCR = RTC_SCCR_Msk;
+
+    /* ----------------------------   RTT  -------------------------------*/
+    //Disable Timer and interrupt
+    uint32_t rtt_mr = RTT_REGS->RTT_MR;
+    RTT_REGS->RTT_MR = rtt_mr & ~(RTT_MR_RTTDIS_Msk | RTT_MR_RTTINCIEN_Msk);
+
+    //Clear status
+    RTT_REGS->RTT_SR;
+
+    /* ----------------------------   RSTC  ------------------------------*/
+    // Disable interrupt
+    uint32_t rstc_mr = RSTC_REGS->RSTC_MR & (RSTC_MR_ENGCLR_Msk |
+                                             RSTC_MR_ERSTL_Msk |
+                                             RSTC_MR_URSTIEN_Msk |
+                                             RSTC_MR_URSTASYNC_Msk |
+                                             RSTC_MR_SCKSW_Msk |
+                                             RSTC_MR_URSTEN_Msk);
+    rstc_mr = rstc_mr & (~RSTC_MR_URSTIEN_Msk);
+    RSTC_REGS->RSTC_MR = RSTC_MR_KEY_PASSWD | rstc_mr;
+
+    /* ----------------------------   PIT  -------------------------------*/
+    //Disable Timer and interrupt
+    uint32_t pit_mr = PIT_REGS->PIT_MR & PIT_MR_PIV_Msk;
+    PIT_REGS->PIT_MR = pit_mr & ~(PIT_MR_PITEN_Msk | PIT_MR_PITIEN_Msk);
+
+    //Clear status
+    PIT_REGS->PIT_SR;
+
+   //Context restore SYSC write protect registers
+   SYSCWP_REGS->SYSCWP_SYSC_WPMR = (SYSCWP_SYSC_WPMR_WPKEY_PASSWD | sysc_wpmr);
+}
+
+
+/* MISRAC 2012 deviation block end */
+
+/*******************************************************************************
+  Function:
+    void SYS_Initialize ( void *data )
+
+  Summary:
+    Initializes the board, services, drivers, application and other modules.
+
+  Remarks:
+ */
+
+void SYS_Initialize ( void* data )
+{
+
+    /* MISRAC 2012 deviation block start */
+    /* MISRA C-2012 Rule 2.2 deviated in this file.  Deviation record ID -  H3_MISRAC_2012_R_2_2_DR_1 */
+
+	SYSC_Disable( );
+
+  
+    CLK_Initialize();
+
+	PIO_Initialize();
+
+
+
+    FLEXCOM7_TWI_Initialize();
+
+    MMU_Initialize();
+
+    AIC_INT_Initialize();
+    
+    /* Disable WDT   */
+    WDT_REGS->WDT_MR = WDT_MR_WDDIS_Msk;
+
+	BSP_Initialize();
+
+
+    /* MISRAC 2012 deviation block start */
+    /* Following MISRA-C rules deviated in this block  */
+    /* MISRA C-2012 Rule 11.3 - Deviation record ID - H3_MISRAC_2012_R_11_3_DR_1 */
+    /* MISRA C-2012 Rule 11.8 - Deviation record ID - H3_MISRAC_2012_R_11_8_DR_1 */
+
+    sysObj.drvAT24 = DRV_AT24_Initialize(DRV_AT24_INDEX, (SYS_MODULE_INIT *)&drvAT24InitData);
+
+
+
+
+    /* MISRAC 2012 deviation block end */
+    APP_Initialize();
+
+
+
+
+    /* MISRAC 2012 deviation block end */
+}
+
+/*******************************************************************************
+ End of File
+*/
--- apps/driver/i2c_eeprom/at24/at24_eeprom_read_write/readme.md
+++ apps/driver/i2c_eeprom/at24/at24_eeprom_read_write/readme.md
@@ -13,7 +13,7 @@ This example application shows how to use the AT24 driver to perform read and wr
 
 ## Description
 
-- This example uses the AT24 driver to communicate with I2C based on-board 24AA02E48 series EEPROM to perform write and read operations in Bare-Metal environment.
+- This example uses the AT24 driver to communicate with I2C based External AT24CM0 series EEPROMs to perform write and read operations in Bare-Metal environment.
 
 ## Downloading and building the application
 
@@ -26,7 +26,7 @@ To build the application, refer to the following table and open the project usin
 
 | Project Name      | Description                                    |
 | ----------------- | ---------------------------------------------- |
-| sam_9x75_eb.X | MPLABX project for [SAM9X75-EB Evaluation Board]() |
+| sam_9x75_curiosity.X | MPLABX project for [SAM9X75-Curiosity Development Board]() |
 |||
 
 ## Setting up AT91Bootstrap loader
@@ -39,25 +39,34 @@ The following table shows the target hardware for the application projects.
 
 | Project Name| Board|
 |:---------|:---------:|
-| sam_9x75_eb.X | [SAM9X75-EB Evaluation Board]() |
+| sam_9x75_curiosity.X | [SAM9X75-Curiosity Development Board]() |
 |||
 
-### Setting up [SAM9X75-EB Evaluation Board]()
+### Setting up [SAM9X75-Curiosity Development Board]()
 
+**ADDTIONAL HARDWARE REQUIRED**
+
+-   One [EEPROM 3 Click](https://www.mikroe.com/eeprom-3-click) board
 
 #### Setting up the board
 
-- Connect the DBGU0 J34 on board to the computer using a UART-FTDI cable (to enable debug com port)
-- Connect the USB port J28 on board to the computer using a micro USB cable (to power the board)
-
+- Connect the USB port J2 on board to the computer using a micro USB cable (to power the board)
+- Connect EEPROM Click 3 Board to MikroBus1 \(J22\) connector on the board as per below Pin Connections table
+   
+   |J22 MikroBus1 Pins |EEPROM 3 CLICK Pins|
+    |------------------|-------------------|
+    |PIN 11 \(PC0\)|SDA|
+    |PIN 12 \(PC1\)|SCL|
+    |PIN 8|GND|
+    |PIN 7|3V3|
 ## Running the Application
 
-1. Build the application using its IDE
+1. Build and program the application using its IDE
 2. LED is turned ON when the data read from EEPROM matches with the written data
 
 Refer to the following table for LED name:
 
 | Board | LED Name |
 | ----- | -------- |
-|  [SAM9X75-EB Evaluation Board]()  | RGB_LED(Green) |
+|  [SAM9X75-Curiosity Development Board]()  | RGB_LED(Green) |
 |||
