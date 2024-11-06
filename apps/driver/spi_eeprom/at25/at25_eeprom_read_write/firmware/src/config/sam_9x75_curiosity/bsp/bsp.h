3ab9b71bb76e5932e155bb22b922c11617581d18
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2024-11-06 18:25:34 +0530

    [DRV_] Regenerated demo for driver__spi_eeprom__at25__at25_eeprom_read_write__firmware__sam_9x75_curiosity.X
    MH3-103516

--- apps/driver/spi_eeprom/at25/at25_eeprom_read_write/firmware/src/config/sam_9x75_curiosity/bsp/bsp.h
+++ apps/driver/spi_eeprom/at25/at25_eeprom_read_write/firmware/src/config/sam_9x75_curiosity/bsp/bsp.h
@@ -72,7 +72,7 @@
 #define LED_RED_On() (PIOC_REGS->PIO_SODR = (1UL<<14))
 #define LED_RED_Off() (PIOC_REGS->PIO_CODR = (1UL<<14))
 /*** SWITCH Macros for USER_BUTTON ***/
-#define USER_BUTTON_Get() ((PIOC_REGS->PIO_PDSR >> 9) & 0x1)
+#define USER_BUTTON_Get() ((PIOC_REGS->PIO_PDSR >> 9) & 0x1U)
 #define USER_BUTTON_STATE_PRESSED 0
 #define USER_BUTTON_STATE_RELEASED 1
 
