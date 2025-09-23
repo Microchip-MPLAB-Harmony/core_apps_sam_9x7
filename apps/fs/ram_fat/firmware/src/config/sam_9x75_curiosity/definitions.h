611f2100559602b47242af500690bf7883f3c18b
Syam Nair <Syam.Nair@microchip.com>
2025-09-23 09:28:21 +0000

    [APP] Regenerated demo for fs__ram_fat__firmware
    MH3C-337

--- apps/fs/ram_fat/firmware/src/config/sam_9x75_curiosity/definitions.h
+++ apps/fs/ram_fat/firmware/src/config/sam_9x75_curiosity/definitions.h
@@ -14,7 +14,7 @@
 
 //DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -59,8 +59,8 @@
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/pio/plib_pio.h"
 #include "peripheral/aic/plib_aic.h"
-#include "driver/memory/drv_memory.h"
 #include "peripheral/ram/plib_ram.h"
+#include "driver/memory/drv_memory.h"
 #include "bsp/bsp.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
