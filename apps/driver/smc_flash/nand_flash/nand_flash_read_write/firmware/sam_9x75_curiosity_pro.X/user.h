fc2699be63ddb637e4a3a029241f8639037fd100
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2026-01-28 16:02:48 +0530

    [DRV] Add smc nand flash read write demo application for SAM 9x75 curiosity pro board
    MH3C-470

---
+++ apps/driver/smc_flash/nand_flash/nand_flash_read_write/firmware/sam_9x75_curiosity_pro.X/user.h
@@ -0,0 +1,202 @@
+/*******************************************************************************
+* Copyright (C) 2024 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+
+/* ************************************************************************** */
+/** Descriptive File Name
+
+  @Company
+    Company Name
+
+  @File Name
+    filename.h
+
+  @Summary
+    Brief description of the file.
+
+  @Description
+    Describe the purpose of this file.
+ */
+/* ************************************************************************** */
+
+#ifndef _EXAMPLE_FILE_NAME_H    /* Guard against multiple inclusion */
+#define _EXAMPLE_FILE_NAME_H
+
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+/* Section: Included Files                                                    */
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+/* This section lists the other files that are included in this file.
+ */
+
+/* TODO:  Include other files here if needed. */
+
+
+/* Provide C++ Compatibility */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+    /* ************************************************************************** */
+    /* ************************************************************************** */
+    /* Section: Constants                                                         */
+    /* ************************************************************************** */
+    /* ************************************************************************** */
+
+    /*  A brief description of a section can be given directly below the section
+        banner.
+     */
+
+
+    /* ************************************************************************** */
+    /** Descriptive Constant Name
+
+      @Summary
+        Brief one-line summary of the constant.
+    
+      @Description
+        Full description, explaining the purpose and usage of the constant.
+        <p>
+        Additional description in consecutive paragraphs separated by HTML 
+        paragraph breaks, as necessary.
+        <p>
+        Type "JavaDoc" in the "How Do I?" IDE toolbar for more information on tags.
+    
+      @Remarks
+        Any additional remarks
+     */
+#define EXAMPLE_CONSTANT 0
+
+
+    // *****************************************************************************
+    // *****************************************************************************
+    // Section: Data Types
+    // *****************************************************************************
+    // *****************************************************************************
+
+    /*  A brief description of a section can be given directly below the section
+        banner.
+     */
+
+
+    // *****************************************************************************
+
+    /** Descriptive Data Type Name
+
+      @Summary
+        Brief one-line summary of the data type.
+    
+      @Description
+        Full description, explaining the purpose and usage of the data type.
+        <p>
+        Additional description in consecutive paragraphs separated by HTML 
+        paragraph breaks, as necessary.
+        <p>
+        Type "JavaDoc" in the "How Do I?" IDE toolbar for more information on tags.
+
+      @Remarks
+        Any additional remarks
+        <p>
+        Describe enumeration elements and structure and union members above each 
+        element or member.
+     */
+    typedef struct _example_struct_t {
+        /* Describe structure member. */
+        int some_number;
+
+        /* Describe structure member. */
+        bool some_flag;
+
+    } example_struct_t;
+
+
+    // *****************************************************************************
+    // *****************************************************************************
+    // Section: Interface Functions
+    // *****************************************************************************
+    // *****************************************************************************
+
+    /*  A brief description of a section can be given directly below the section
+        banner.
+     */
+
+    // *****************************************************************************
+    /**
+      @Function
+        int ExampleFunctionName ( int param1, int param2 ) 
+
+      @Summary
+        Brief one-line description of the function.
+
+      @Description
+        Full description, explaining the purpose and usage of the function.
+        <p>
+        Additional description in consecutive paragraphs separated by HTML 
+        paragraph breaks, as necessary.
+        <p>
+        Type "JavaDoc" in the "How Do I?" IDE toolbar for more information on tags.
+
+      @Precondition
+        List and describe any required preconditions. If there are no preconditions,
+        enter "None."
+
+      @Parameters
+        @param param1 Describe the first parameter to the function.
+    
+        @param param2 Describe the second parameter to the function.
+
+      @Returns
+        List (if feasible) and describe the return values of the function.
+        <ul>
+          <li>1   Indicates an error occurred
+          <li>0   Indicates an error did not occur
+        </ul>
+
+      @Remarks
+        Describe any special behavior not described above.
+        <p>
+        Any additional remarks.
+
+      @Example
+        @code
+        if(ExampleFunctionName(1, 2) == 0)
+        {
+            return 3;
+        }
+     */
+    int ExampleFunction(int param1, int param2);
+
+
+    /* Provide C++ Compatibility */
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _EXAMPLE_FILE_NAME_H */
+
+/* *****************************************************************************
+ End of File
+ */
