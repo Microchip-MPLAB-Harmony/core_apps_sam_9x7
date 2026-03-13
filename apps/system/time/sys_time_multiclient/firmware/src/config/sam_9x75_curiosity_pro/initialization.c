fb8d4abb3f96e5efaec6a2579ac44f944072d6ce
Kevin Abishek S <kevinabishek.s@microchip.com>
2026-03-13 12:53:41 +0530

    [APPS] Regenerated sys_time_multiclient demo for sam_9x75_curiosity_pro MH3C-576

--- apps/system/time/sys_time_multiclient/firmware/src/config/sam_9x75_curiosity_pro/initialization.c
+++ apps/system/time/sys_time_multiclient/firmware/src/config/sam_9x75_curiosity_pro/initialization.c
@@ -234,12 +234,12 @@ void SYS_Initialize ( void* data )
     /* Disable WDT   */
     WDT_REGS->WDT_MR = WDT_MR_WDDIS_Msk;
 
-    DBGU_Initialize();
-
  
     TC0_CH0_TimerInitialize(); 
      
     
+    DBGU_Initialize();
+
 	BSP_Initialize();
 
     /* MISRAC 2023 deviation block start */
@@ -248,11 +248,11 @@ void SYS_Initialize ( void* data )
     /* MISRA C-2023 Rule 11.8 - Deviation record ID - H3_MISRAC_2023_R_11_8_DR_1 */
 
 
-    /* MISRA C-2012 Rule 11.3, 11.8 deviated below. Deviation record ID -  
-    H3_MISRAC_2012_R_11_3_DR_1 & H3_MISRAC_2012_R_11_8_DR_1*/
-        
-    sysObj.sysTime = SYS_TIME_Initialize(SYS_TIME_INDEX_0, (SYS_MODULE_INIT *)&sysTimeInitData);
+    /* MISRA C-2023 Rule 11.3, 11.8 deviated below. Deviation record ID -
+    H3_MISRAC_2023_R_11_3_DR_1 & H3_MISRAC_2023_R_11_8_DR_1*/
     
+    sysObj.sysTime = SYS_TIME_Initialize(SYS_TIME_INDEX_0, (SYS_MODULE_INIT *)&sysTimeInitData);
+
     /* MISRAC 2012 deviation block end */
 
 
