c5512ffdd668f32689259def7b2aa5b08491b0f6
Pranjal Das <pranjal.das@microchip.com>
2026-04-30 23:24:13 +0530

    [DRV_SDMMC] Regenerated demo for apps__driver__sdmmc__async__sdio_read_write__sam_9x75_curiosity.X
    MH3C-855

--- apps/driver/sdmmc/async/sdio_read_write/src/config/default/driver/sdmmc/src/drv_sdmmc.c
+++ apps/driver/sdmmc/async/sdio_read_write/src/config/default/driver/sdmmc/src/drv_sdmmc.c
@@ -146,7 +146,7 @@ static DRV_SDMMC_BUFFER_OBJ* lDRV_SDMMC_FreeBufferObjectGet(DRV_SDMMC_CLIENT_OBJ
     }
     return NULL;
 }
-/* MISRA C-2012 Rule 11.3 deviated:12 Deviation record ID -  H3_MISRAC_2012_R_11_3_DR_1 */
+/* MISRA C-2023 Rule 11.3 deviated:12 Deviation record ID -  H3_MISRAC_2023_R_11_3_DR_1 */
 
 static bool lDRV_SDMMC_BufferObjectAddToList(
     DRV_SDMMC_OBJ* dObj,
@@ -346,7 +346,7 @@ static void lDRV_SDMMC_TimerCallback (
     *timeoutFlag = true;
 }
 
-/* MISRA C-2012 Rule 14.3 deviated:2 Deviation record ID -  H3_MISRAC_2012_R_14_3_DR_1 */
+/* MISRA C-2023 Rule 14.3 deviated:2 Deviation record ID -  H3_MISRAC_2023_R_14_3_DR_1 */
 static void lDRV_SDMMC_InitCardContext ( uint32_t drvIndex, DRV_SDHOST_CARD_CTXT* cardCtxt )
 {
     cardCtxt->isAttached            = false;
@@ -462,8 +462,8 @@ static void lDRV_SDMMC_ParseCSD (
         cardCtxt->discCapacity = ((cSizeVal + 1U) * (1UL << (cSizeMult + 2U)) * (1UL << readBlockLength)) / 512U;
     }
 }
-/* MISRA C-2012 Rule 16.1, 16.3 and 16.5 deviated below.
-  Deviation record ID -  H3_MISRAC_2012_R_16_1_DR_1, H3_MISRAC_2012_R_16_3_DR_1 & H3_MISRAC_2012_R_16_5_DR_1 */
+/* MISRA C-2023 Rule 16.1, 16.3 and 16.5 deviated below.
+  Deviation record ID -  H3_MISRAC_2023_R_16_1_DR_1, H3_MISRAC_2023_R_16_3_DR_1 & H3_MISRAC_2023_R_16_5_DR_1 */
 
 
 static void lDRV_SDMMC_CommandSend (
@@ -828,10 +828,10 @@ static uint8_t lDRV_SDMMC_SDIO_CIS_Tuple_Rd(
                     }
 
                 break;
-                
+
                 default:
                     //Do nothing
-                
+
                 break;
             }
         }
@@ -1892,7 +1892,7 @@ static uint8_t lDRV_SDMMC_SendSCR_ACMD51(DRV_SDMMC_OBJ* dObj, uint8_t* scrBuffer
                 }
             }
             break;
-            
+
         default:
             //Do Nothing
             break;
@@ -2960,7 +2960,7 @@ static void lDRV_SDMMC_MediaInitialize (
 // *****************************************************************************
 // *****************************************************************************
 
-/* MISRA C-2012 Rule 11.8 deviated:2 Deviation record ID -  H3_MISRAC_2012_R_11_8_DR_1 */
+/* MISRA C-2023 Rule 11.8 deviated:2 Deviation record ID -  H3_MISRAC_2023_R_11_8_DR_1 */
 
 SYS_MODULE_OBJ DRV_SDMMC_Initialize (
     const SYS_MODULE_INDEX drvIndex,
@@ -3526,7 +3526,7 @@ void DRV_SDMMC_Async_SDIO_Read (
             opType = DRV_SDMMC_OP_TYP_SDIO_RD_BYTES;
         }
     }
-    
+
     if (err == true)
     {
         if (commandHandle != NULL)
@@ -3687,7 +3687,7 @@ SYS_MEDIA_GEOMETRY* DRV_SDMMC_GeometryGet (
 
     return mediaGeometryObj;
 }
-/* MISRA C-2012 Rule 11.1 deviated:1 Deviation record ID -  H3_MISRAC_2012_R_11_1_DR_1 */
+/* MISRA C-2023 Rule 11.1 deviated:1 Deviation record ID -  H3_MISRAC_2023_R_11_1_DR_1 */
 
 void DRV_SDMMC_EventHandlerSet (
     const DRV_HANDLE handle,
@@ -3767,7 +3767,7 @@ void DRV_SDMMC_Tasks( SYS_MODULE_OBJ object )
     DRV_SDMMC_CLIENT_OBJ* clientObj = NULL;
     DRV_SDMMC_BUFFER_OBJ* currentBufObj = NULL;
     DRV_SDMMC_EVENT evtStatus = DRV_SDMMC_EVENT_COMMAND_COMPLETE;
-    uint32_t response = 0;    
+    uint32_t response = 0;
     static bool cardAttached = true;
 
     dObj = &gDrvSDMMCObj[object];
@@ -4297,7 +4297,7 @@ void DRV_SDMMC_Tasks( SYS_MODULE_OBJ object )
                         else
                         {
                             //Do noting
-                        }        
+                        }
                     }
 
                     if (dObj->dataTransferFlags.isDataPresent == true)
