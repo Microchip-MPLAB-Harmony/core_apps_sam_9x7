5e5cad9dfde576d9dfefe82424ccda5480bee64a
Pranjal Das <pranjal.das@microchip.com>
2026-04-30 23:11:17 +0530

    [SYS_FS] Regenerated demo for apps__fs__sdmmc_fat__firmware__sam_9x75_curiosity.X
    MH3C-855

--- apps/fs/sdmmc_fat/firmware/src/config/sam_9x75_curiosity/driver/sdmmc/src/drv_sdmmc_file_system.c
+++ apps/fs/sdmmc_fat/firmware/src/config/sam_9x75_curiosity/driver/sdmmc/src/drv_sdmmc_file_system.c
@@ -54,7 +54,7 @@
 // *****************************************************************************
 // *****************************************************************************
 
-/* MISRA C-2012 Rule 11.1 deviated:5 Deviation record ID -  H3_MISRAC_2012_R_11_1_DR_1 */
+/* MISRA C-2023 Rule 11.1 deviated:5 Deviation record ID -  H3_MISRAC_2023_R_11_1_DR_1 */
 
 static const SYS_FS_MEDIA_FUNCTIONS sdmmcMediaFunctions =
 {
